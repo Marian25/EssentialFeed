793b2f3889ec977533144ac1e70779ab566cfe4a
Marian25 <marian.stanciulica@gmail.com>
2022-09-30 18:49:47 +0300

    FeedItem Added

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Marian Stanciulica on 30.09.2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
