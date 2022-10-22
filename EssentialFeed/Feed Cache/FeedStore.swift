26e52936390a58dfea95d03e0ba2aa2467c791bb
Marian25 <marian.stanciulica@gmail.com>
2022-10-22 19:30:04 +0300

    FeedStore moved to its own file

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,14 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Marian Stanciulica on 22.10.2022.
+//
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
