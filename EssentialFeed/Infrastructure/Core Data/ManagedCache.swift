e17b33c385b03a1c0b1f4e4f8ff4202c469966de
Marian25 <marian.stanciulica@gmail.com>
2022-10-27 20:33:49 +0300

    Extract CoreData managed model classes into separate files

---
+++ EssentialFeed/Infrastructure/Core Data/ManagedCache.swift
@@ -0,0 +1,46 @@
+//
+//  ManagedCache.swift
+//  EssentialFeed
+//
+//  Created by Marian Stanciulica on 27.10.2022.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ManagedCache> {
+        return NSFetchRequest<ManagedCache>(entityName: "ManagedCache")
+    }
+    
+    var localFeed: [LocalFeedImage] {
+        feed
+            .compactMap { $0 as? ManagedFeedImage }
+            .map { $0.local }
+    }
+    
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        NSOrderedSet(array: localFeed.map {
+            let managedFeedImage = ManagedFeedImage(context: context)
+            managedFeedImage.id = $0.id
+            managedFeedImage.imageDescription = $0.description
+            managedFeedImage.location = $0.location
+            managedFeedImage.url = $0.url
+            return managedFeedImage
+        })
+    }
+}
---
+++ EssentialFeed/Infrastructure/Core Data/ManagedFeedImage.swift
@@ -0,0 +1,25 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Marian Stanciulica on 27.10.2022.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+class ManagedFeedImage: NSManagedObject {
+    @NSManaged public var id: UUID
+    @NSManaged public var imageDescription: String?
+    @NSManaged public var location: String?
+    @NSManaged public var url: URL
+    @NSManaged public var cache: ManagedCache
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ManagedFeedImage> {
+        return NSFetchRequest<ManagedFeedImage>(entityName: "ManagedFeedImage")
+    }
+    
+    var local: LocalFeedImage {
+        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+    }
+}
