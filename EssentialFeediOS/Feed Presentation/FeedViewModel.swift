03e8d7d0c18ec4e7e59468f86e712086e63bdb97
Marian25 <marian.stanciulica@gmail.com>
2023-01-05 16:34:31 +0200

    Move 'FeedViewModel' to a separate file

---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Marian Stanciulica on 05.01.2023.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
