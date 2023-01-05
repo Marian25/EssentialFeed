77ce9139a2b831d364eb1de46c3af6e62d144235
Marian25 <marian.stanciulica@gmail.com>
2023-01-05 16:15:54 +0200

    'FeedErrorView' move to its own file

---
+++ EssentialFeediOS/Feed UI/Views/FeedErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Marian Stanciulica on 05.01.2023.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
