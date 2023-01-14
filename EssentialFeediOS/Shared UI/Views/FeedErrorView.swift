f1cbe90e2eac9b2fd1d61cf544a39bdc35f49bb8
Marian25 <marian.stanciulica@gmail.com>
2023-01-14 16:20:01 +0200

    Replace hardcoded fonts with dynamic fonts

--- EssentialFeediOS/Shared UI/Views/FeedErrorView.swift
+++ EssentialFeediOS/Shared UI/Views/FeedErrorView.swift
@@ -31,7 +31,7 @@ public final class ErrorView: UIButton {
 
         var attributes = AttributeContainer()
         attributes.paragraphStyle = paragraphStyle
-        attributes.font = UIFont.preferredFont(forTextStyle: .body)
+        attributes.font = .preferredFont(forTextStyle: .body)
         return attributes
     }
     
