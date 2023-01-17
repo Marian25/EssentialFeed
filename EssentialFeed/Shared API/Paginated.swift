06964e143680b77ca12bf16f33548b9c69cdfc37
Marian25 <marian.stanciulica@gmail.com>
2023-01-17 22:09:42 +0200

    Does not load more after loading last page

--- EssentialFeed/Shared API/Paginated.swift
+++ EssentialFeed/Shared API/Paginated.swift
@@ -6,12 +6,12 @@
 //
 
 public struct Paginated<Item> {
-    public typealias LoadMoreCompletion = (Result<Paginated<Item>, Error>) -> Void
+    public typealias LoadMoreCompletion = (Result<Self, Error>) -> Void
     
     public let items: [Item]
     public let loadMore: ((@escaping LoadMoreCompletion) -> Void)?
     
-    public init(items: [Item], loadMore: ((LoadMoreCompletion) -> Void)? = nil) {
+    public init(items: [Item], loadMore: ((@escaping LoadMoreCompletion) -> Void)? = nil) {
         self.items = items
         self.loadMore = loadMore
     }
